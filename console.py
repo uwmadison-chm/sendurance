4ab10655c7866785f598fde3770401aa310cc987
Dan Fitch <dgfitch@gmail.com>
2019-04-17 12:51:02 -0500

    Moving client secret

--- console.py
+++ console.py
@@ -1,7 +1,14 @@
 from fitbit_api import FitbitApi
+import json
 import sys
 
-fitbit = FitbitApi(sys.argv[1])
+try:
+    with open("client.json") as json_file:  
+        client = json.load(json_file)
+except FileNotFoundError as e:
+    sys.exit("You need to make sure there is a client.json file in the current directory with an 'id' and 'secret' keys'.")
+
+fitbit = FitbitApi(sys.argv[1], client['id'], client['secret'])
 
 import IPython; IPython.embed()
 
