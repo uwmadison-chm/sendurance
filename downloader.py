7c968a551ea4f0c9d0ea85b41379990c2e10775d
Sam Westby <samcwestby@gmail.com>
2019-07-01 13:55:26 -0500

    Updated downloader.py to use DownloadWrapper

--- downloader.py
+++ downloader.py
@@ -3,14 +3,11 @@ import json
 import sys
 import csv
 import argparse
-
-from datetime import datetime, timedelta
+from datetime import datetime
 import dateutil.parser
-import os
-import os.path
+import coloredlogs
+from DownloadWrapper import DownloadWrapper
 
-import logging                                                                                                                                                                                                                 
-import coloredlogs 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', action='count')
@@ -34,73 +31,14 @@ try:
 except FileNotFoundError as e:
     sys.exit("You need to make sure there is a client.json file in the current directory with an 'id' and 'secret' keys'.")
 
-def convert_time(day, time):
-    time = datetime.strptime(time, '%H:%M:%S').time()
-    day = day.replace(hour=time.hour, minute=time.minute, second=time.second)
-    return day.isoformat()
-
-def save_sleep_day(writer, fitbit, ppt, day):
-    sleep = fitbit.sleep(day)
-    if not sleep['sleep']:
-        return
-    intra = sleep['sleep'][0]['minuteData']
-    interpreter = ['', 'Aleep', 'Restless', 'Awake']
-    for item in intra:
-        writer.writerow([ppt, convert_time(day, item['dateTime']), item['value'], interpreter[int(item['value'])]])
-
-def save_sleep(fitbit, ppt, start, end, output=args.output):
-    path = os.path.join(output, 'sleep')
-    os.makedirs(path, exist_ok=True)
-    with open(os.path.join(path, ppt + '_1min_sleep.tsv'), 'w') as tsvfile:
-        writer = csv.writer(tsvfile, dialect='excel-tab', lineterminator='\n')
-        writer.writerow(["ID", "Time", "State", "Interpreted"])
-        for day in [start + timedelta(days=x) for x in range(0, (end-start).days + 1)]:
-            logging.info(f"Downloading {day} for {ppt} from {email}")
-            save_sleep_day(writer, fitbit, ppt, day)
-
-def save_steps_day(writer, fitbit, ppt, day):
-    steps = fitbit.steps(day)
-    if not steps['activities-steps-intraday']:
-        return
-    intra = steps['activities-steps-intraday']['dataset']
-    for item in intra[1:2]:
-        writer.writerow([ppt, convert_time(day, item['time']), item['value']])
-
-def save_steps(fitbit, ppt, start, end, output=args.output):
-    path = os.path.join(output, 'activity')
-    os.makedirs(path, exist_ok=True)
-    with open(os.path.join(path, ppt + '_1min_steps.tsv'), 'w') as tsvfile:
-        writer = csv.writer(tsvfile, dialect='excel-tab', lineterminator='\n')
-        writer.writerow(["ID", "Time", "Value"])
-        for day in [start + timedelta(days=x) for x in range(0, (end-start).days + 1)]:
-            logging.info(f"Downloading {day} for {ppt} from {email}")
-            save_steps_day(writer, fitbit, ppt, day)
-
-def save_hrv_day(writer, fitbit, ppt, day):
-    hrv = fitbit.hrv(day)
-    if not hrv['activities-heart-intraday']:
-        return
-    intra = hrv['activities-heart-intraday']['dataset']
-    for item in intra:
-        writer.writerow([ppt, convert_time(day, item['time']), item['value']])
-
-def save_hrv(fitbit, ppt, start, end, output=args.output):
-    path = os.path.join(output, 'HR')
-    os.makedirs(path, exist_ok=True)
-    with open(os.path.join(path, ppt + '_1min_HR.tsv'), 'w') as tsvfile:
-        writer = csv.writer(tsvfile, dialect='excel-tab', lineterminator='\n')
-        writer.writerow(["ID", "Time", "Heartrate"])
-        for day in [start + timedelta(days=x) for x in range(0, (end-start).days + 1)]:
-            logging.info(f"Downloading {day} for {ppt} from {email}")
-            save_hrv_day(writer, fitbit, ppt, day)
-
 with open(args.input, newline='') as csvfile:
     r = csv.reader(csvfile)
     for row in r:
         ppt = row[0]
         email = row[1]
         fitbit = FitbitApi(email, client['id'], client['secret'])
-
-        # save_hrv(fitbit, ppt, args.start_date, args.end_date)
-        # save_sleep(fitbit, ppt, args.start_date, args.end_date)
-        # save_steps(fitbit, ppt, args.start_date, args.end_date)
+        Downloader = DownloadWrapper(ppt=ppt, fitbit=fitbit, start=args.start_date,
+                                     end=args.end_date, output=args.output)
+        # Downloader.save_hrv()
+        # Downloader.save_sleep()
+        # Downloader.save_steps()
